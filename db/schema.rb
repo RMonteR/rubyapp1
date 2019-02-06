559faa0430d05cb520289a755f32c3e46499b4e8
RMonteR <talktotheowl@inventati.org>
2019-02-06 15:52:10 +0100

    add color field to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_084523) do
+ActiveRecord::Schema.define(version: 2019_02_06_103537) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_05_084523) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "colour"
   end
 
 end
