a26bcdfaccad3c150b47de37bfc6c977be0e809b
RMonteR <talktotheowl@inventati.org>
2019-02-18 11:05:23 +0100

    5.3 add contact form and set up Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,18 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about' # how do I say - if you are instructed to get this about page, redirect the user to contact? (for example because about is under reconstruction)
   get 'simple_pages/contact'
+  post 'simple_pages/thank_you'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#index'
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
   root 'orders#index'
