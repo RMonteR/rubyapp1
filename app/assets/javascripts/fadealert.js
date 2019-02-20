ca95c8a45c1f390cc21db6b213e667de8e976063
RMonteR <talktotheowl@inventati.org>
2019-02-20 09:38:07 +0100

    add js file

---
+++ app/assets/javascripts/fadealert.js
@@ -0,0 +1,8 @@
+// Bonus task 5.5
+
+// You might not want to have the alerts and notices visible all the time.
+// So you could get them to fade out after a certain amount of time.
+//
+// You can use jQuery to select all elements with the class .alert.
+// You can set the time for how long they should be visible with the .delay() function.
+// The .fadeOut() function added to the same line right after the .delay() function will let you fade out your alerts.
