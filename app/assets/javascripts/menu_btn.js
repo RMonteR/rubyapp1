9c1b99a1d6a3e8fad41a5462db2b588a310b72b9
RMonteR <talktotheowl@inventati.org>
2019-02-28 11:52:19 +0100

    add turbolinks:load to script

--- app/assets/javascripts/menu_btn.js
+++ app/assets/javascripts/menu_btn.js
@@ -3,14 +3,16 @@
 // # metti content su col-8
 // # check https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
 
-const $menuBtn = $("#menu-btn");
+$(document).on('turbolinks:load', function() {
+  const $menuBtn = $("#menu-btn");
 
-// if viewport is less than md:
-
-  $menuBtn.on("click", function() {
-    $("#removable-sidebar").removeClass(".d-none");
-    $("#removable-sidebar").addClass(".col-4");
-    $(".content").addClass("col-8");
+  // if viewport is less than md:
+  if (window.innerWidth <= 576) {
+    $menuBtn.on("click", function() {
+      $("#removable-sidebar").removeClass(".d-none");
+      $("#removable-sidebar").addClass(".col-4");
+      $(".content").addClass("col-8");
     });
-
-//  remove background-color for .button-col    
+  };
+//  remove background-color for .button-col
+});
