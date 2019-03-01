84f8ba85ce0485f353b549efd145f8588a030666
RMonteR <talktotheowl@inventati.org>
2019-03-01 09:37:30 +0100

    5.11 last fixes

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -10,21 +10,16 @@ class SimplePagesController < ApplicationController
     redirect_to simple_pages_contact_path
   end
 
-  # def thank_you
-  #   @name = params[:name]
-  #   @email = params[:email]
-  #   @message = params[:message]
-  #   ActionMailer::Base.mail(
-  #     from: @email,
-  #     to: 'your-email@example.com',
-  #     subject: 'A new contact form message from #{@name}',
-  #     body: @message).deliver_now
-  #   end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end  
 
   def create
     super
     if @user.persisted?
-      UserMailer.contact_form(@user).deliver_now
     end
   end
 end
