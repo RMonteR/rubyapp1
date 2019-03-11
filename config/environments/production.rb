a2698fba1857f6ac7f29ed9c250f942728dada3b
RMonteR <talktotheowl@inventati.org>
2019-03-11 08:53:19 +0100

    6.13 add product channel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+config.web_socket_server_url = "wss://madeupbikes.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://madeupbikes.herokuapp.com', 'http://madeupbikes.herokuapp.com']
 end
